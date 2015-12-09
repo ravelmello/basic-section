78f084ba19648edfae97c5f770682cb6226c0a58
ravelmello <ravelmello@hotmail.com>
2015-12-08 23:12:36 -0300

    Corrigindo o Vacilo

--- index.php
+++ index.php
@@ -4,12 +4,9 @@ require 'vendor/autoload.php';
 session_start();
 
 if ((!isset($_SESSION['login'])==true) && (!isset($_SESSION['password'])==true)) {
-
-
     unset($_SESSION['login']);
     unset($_SESSION['password']);
     header('location:login.php');
-
 }
 
 $logado = $_SESSION['login'];
